8bdc6d3830c4157c53a04e296731cba225a16fd2
ifechukwudei <nwokoifechukwu@gmail.com>
2022-09-10 16:38:17 +0100

    finish

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <time.h>
+#include <stdio.h>
+/*
+ * 3-print_alphabets.c
+ *
+ * Description:
+ * This program prints
+ * all the lower case characters in
+ * the alphabet, then the uppercase
+ *  using putchar()
+ *
+ */
+
+/**
+  * main - this is the main function that runs everything
+  * @void: no arguments passed in
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char letter;
+	int i;
+
+	letter = 'a';
+
+	for (i = 0; i < 26; i++)
+		putchar(letter + i);
+	letter = 'A';
+	for (i = 0; i < 26; i++)
+		putchar(letter + i);
+	putchar('\n');
+	return (0);
+}
